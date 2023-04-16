eb745d1302caeec657b96180d47636c575d05cf2
Pavel <pashok123321@inbox.ru>
2023-04-16 19:29:16 +0500

    update task 3

--- Program.cs
+++ Program.cs
@@ -8,15 +8,68 @@ namespace GetResult
         {
             while (true)
             {
-                Console.WriteLine("Ввод: ");
+                Console.WriteLine("Input: ");
                 string input = Console.ReadLine();
                 string ouput = GetResults(input);
-                Console.WriteLine("Вывод: " + ouput);
+                Console.WriteLine("Output: " + ouput);
 
             }
         }
+        static string CountRepeatSymbols(string str)
+        {
+            string seached = "";
+            string witholdSymbols = str;
+            string newString;
+            int count;
+            char currentSearch;
+
+            while (true)
+            {
+
+                count = 0;
+                newString = "";
+                currentSearch = witholdSymbols[0];
+                foreach (char c in witholdSymbols)
+                {
+                    if (c == currentSearch)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        newString += c;
+                    }
+                }
+                if (count >= 1)
+                {
+                    seached += currentSearch + " - " + count + ", ";
+                }
+                witholdSymbols = newString;
+                if (newString == "")
+                {
+                    return seached;
+                }
+            }
+        }
+
         static string GetResults(string input)
         {
+            string result;
+            string correct = "abcdefghijklmnopqrstuvwxyz";
+            string notCorrect = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (correct.IndexOf(input[i]) == -1)
+                {
+                    notCorrect += input[i];
+
+                }
+
+            }
+            if (notCorrect.Length > 0)
+            {
+                return "Wrong Symbols: " + notCorrect;
+            }
             if (input.Length % 2 == 0)
             {
                 int half = input.Length / 2;
@@ -28,17 +81,23 @@ namespace GetResult
                 char[] resultsSecond = second.ToCharArray();
                 Array.Reverse(resultsSecond);
                 string resultSecond = new string(resultsSecond);
-                string getresult = resultsFirst + resultSecond;
-                return getresult;
+                result = resultsFirst + resultSecond;
+
             }
             else
             {
                 char[] resultElse = input.ToCharArray();
                 Array.Reverse(resultElse);
                 string reversed = new string(resultElse);
-                string resultReverse = reversed + input;
-                return resultReverse;
+                result = reversed + input;
+
             }
+            string doubleSymbols = CountRepeatSymbols(result);
+            if (doubleSymbols != "")
+            {
+                Console.WriteLine("Count symbols " + doubleSymbols);
+            }
+            return result;
         }
     }
 }
